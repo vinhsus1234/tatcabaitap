406d7d3e051eab9cfafebb7460469fe4769bcb01
vinhsus1234 <vinhsus1234@gmail.com>
2023-06-23 09:01:14 +0700

    bai tap C++

---
+++ tubai52toi61.cpp
@@ -0,0 +1,151 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//bai 52
+int main() {
+//bai 52
+//    int n;
+//    cin >> n;
+//    int chuso = 0;
+//    int sonhonhat = 0;
+//    while(n != 0) {
+//        sonhonhat = 0;
+//        chuso = n % 10;
+//        if (sonhonhat < chuso) {
+//            sonhonhat = chuso;
+//        }
+//        n = n / 10;
+//    }
+//    cout << sonhonhat;
+//bai 53
+//////    int n;
+//////    cin >> n;
+//////    int chuso = 0;
+//////    int socaonhat = 0;
+//////    int dem = 0;
+//////    while(n != 0) {
+//////        chuso = n % 10;
+//////
+//////        if (socaonhat < chuso) {
+//////            socaonhat = chuso;
+//////        }if (chuso == socaonhat) {
+//////            dem+=1;
+//////        }
+//////        n = n / 10;
+//////    }
+//////    cout << socaonhat << "\n";
+//////    cout << dem;
+// bai 54
+//    int n;
+//    cin >> n;
+//    int chuso = 0;
+//    int sonhonhat = 0;
+//    int dem = 0;
+//    while(n != 0) {
+//        chuso = n % 10;
+//        sonhonhat = 0;
+//        if (sonhonhat < chuso) {
+//            sonhonhat = chuso;
+//        }if (chuso == sonhonhat) {
+//            dem += 1;
+//        }
+//        n = n / 10;
+//    }
+//    cout << sonhonhat << "\n";
+//    cout << dem;
+//bai 55
+//    int n;
+//    cin >> n;
+//    int chuso = 0;
+//    int soluongchusodautien = 0;
+//    while (n != 0) {
+//        chuso = n % 10;
+//        soluongchusodautien += 1;
+//        n = n / 10;
+//    }
+//    cout << "so luong chu so dau tien la: " << soluongchusodautien;
+//bai 56
+//    int n;
+//    cin >> n;
+//    int chuso = 0;
+//    bool sole = true;
+//    while (n != 0) {
+//        if ((n % 10) % 2 == 1) {
+//            sole = false;
+//            return 0;
+//        }
+//
+//        if (sole == true) {
+//            cout << "Dung";
+//        }
+//        else if (sole == false){
+//            cout << "khong";
+//        }
+//        n = n / 10;
+//    }
+
+//bai 57, 58
+//    int n;
+//    cin >> n;
+//    int chuso = 0;
+//    bool sole = true;
+//    while (n != 0) {
+//        if ((n % 10) % 2 == 0) {
+//            sole = false;
+//            return 0;
+//        }
+//
+//        if (sole == true) {
+//            cout << "Dung";
+//        }
+//        else if (sole == false){
+//            cout << "khong";
+//        }
+//        n = n / 10;
+//    }
+//    return 0;
+//Vinh khong biet lam bai 59.
+//bai 60
+//    int n;
+//    bool tangdan = true;
+//    cin >> n;
+//    int chusodau = n % 10;
+//    n = n /10;
+//    while(n != 0) {
+//        int chusocuoi = n % 10;
+//        if (chusodau < chusocuoi) {
+//            tangdan = false;
+//        }else {
+//            chusodau = chusocuoi;
+//        }
+//        n = n / 10;
+//    }
+//    if (tangdan == true) {
+//        cout << "Dung";
+//    }else{
+//        cout << "sai";
+//    }
+
+//bai 61
+//    int n;
+//    bool giamdan = true;
+//    cin >> n;
+//    int chusodau = n % 10;
+//    n = n /10;
+//    while(n != 0) {
+//        int chusocuoi = n % 10;
+//        if (chusocuoi < chusodau) {
+//            giamdan = false;
+//        }else {
+//            chusodau = chusocuoi;
+//        }
+//        n = n / 10;
+//    }
+//    if (giamdan == true) {
+//        cout << "Dung";
+//    }else{
+//        cout << "sai";
+//    }
+
+}
