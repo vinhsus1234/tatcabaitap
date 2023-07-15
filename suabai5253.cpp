1a18112f0f7044b3ed4a7f17b7b4a1b8e3c6ca99
vinhsus1234 <vinhsus1234@gmail.com>
2023-07-15 07:34:21 +0700

    baitap

---
+++ suabai5253.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    //bai 52
+//    int n; int chuso;
+//    int chusonhonhat;
+//    cin >> n;
+//    chusonhonhat = n % 10;
+//    while(n != 0) {
+//        chuso = n % 10;
+//        if (chuso < chusonhonhat) {
+//            chusonhonhat = chuso;
+//        }
+//        n = n / 10;
+//    }
+//    n = n / 10;
+//    cout << chusonhonhat;
+//
+//    int n;
+//    cin >> n;
+//    int chuso = 0;
+//    int socaonhat;
+//    int dem = 1;
+//    socaonhat = 0;
+//    while(n != 0) {
+//        chuso = n % 10;
+//    if (chuso == socaonhat) {
+//            dem+=1;
+//        }else if (chuso > socaonhat) {
+//            socaonhat = chuso;
+//        }
+//        n = n / 10;
+//    }
+//    cout << "so cao nhat la " << socaonhat << "\n";
+//    cout << dem;
+}
