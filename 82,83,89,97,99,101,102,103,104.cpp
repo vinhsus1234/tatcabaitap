441407489c45c4a2b969afb485ea01276d9a099b
vinhsus1234 <vinhsus1234@gmail.com>
2023-07-15 07:51:56 +0700

    baitap

--- 82,83,89,97,99,101,102,103,104.cpp
+++ 82,83,89,97,99,101,102,103,104.cpp
@@ -231,23 +231,6 @@ int main()
 //
 //    cout << "ngay truoc do la " << ngay << " " << thang << " " << nam;
 
-// bai 104
-    int day, month, year, s, i;
-    cout << "\nNhap ngay, thang, nam: ";
-    cin >> day >> month >> year;
-    s = day;
-    for(i = 1; i < month; i++)
-    {
-        switch (i)
-        {
-            case 4: case 6: case 9 : case 11: s+= 30; break;
-            case 2: s += (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) ? 29 : 28;
-                break;
-            default: s += 31;
-        }
-    }
-    cout << "\nNgay thu " << s << " trong nam";
-    return 0;
-
+// bai 104 vinh không làm được
 }
 
